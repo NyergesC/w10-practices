38540402b93ace508d7553532477768f58d521c2
NyergesC <cintia.nyerges@gmail.com>
2022-03-23 19:58:52 +0100

    css

--- script.js
+++ script.js
@@ -3,15 +3,15 @@ fetch("beers.json")
     .then(response => response.json())
     .then(data => {   
         for (const beer of data.cards) {
-            document.getElementById('root').innerText += `
-                        ${beer.title}
-                        ${beer.sub}
-                        ${beer.text}
-                    `
-            
-            }
-
-
+            document.getElementById('container').insertAdjacentHTML("beforeend",  `
+                    <div class="cards">
+                        <h2>${beer.title}</h2><br>
+                        <p>${beer.sub}</p><br>
+                        <p> ${beer.text}</p><br>
+                    </div>
+                    `            
+            )}
+       
         })
         
     
